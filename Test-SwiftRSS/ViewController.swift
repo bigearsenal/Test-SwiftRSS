d1bbb0862e35201a6d916e6ebf2fcbd2cd960d64
Chung Tran <Ivan@Chungs-Mac-Pro.local>
2014-10-19 03:48:15 +0400

    Initial Commit

---
+++ Test-SwiftRSS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test-SwiftRSS
+//
+//  Created by Chung Tran on 19/10/14.
+//  Copyright (c) 2014 Chung Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
